d3076d0aea3d162bea3661f35d7f4460ad914524
tianye <ye.tian@acadine.com>
2017-12-14 15:18:14 +0800

    add mongo authentication

--- mongo_conn.py
+++ mongo_conn.py
@@ -6,11 +6,16 @@ import env_setup
 
 host = os.environ.get("MONGO_HOST")
 port = os.environ.get("MONGO_PORT")
-
+username = os.environ.get("MONGO_USER")
+password = os.environ.get("MONGO_PASSWORD")
 
 def get_mongo_conn ():
   '''
     get mongodb connection
   '''
 
-  return MongoClient('mongodb://%s:%s/'%(host, port) )
+  uri = 'mongodb://%s:%s/'%(host, port)
+  if username and password:
+    uri = 'mongodb://%s:%s@%s:%s/'%(host, port)
+
+  return MongoClient(uri)
